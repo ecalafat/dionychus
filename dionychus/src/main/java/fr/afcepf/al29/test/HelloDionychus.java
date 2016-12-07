8b848841c4715d5f34e74b18a08891d2a0c4cc1d
chienDesbois <vincentzanol@gmail.com>
2016-12-07 09:37:46 +0100

    UTF-8

--- dionychus/src/main/java/fr/afcepf/al29/test/HelloDionychus.java
+++ dionychus/src/main/java/fr/afcepf/al29/test/HelloDionychus.java
@@ -3,7 +3,7 @@ package fr.afcepf.al29.test;
 public class HelloDionychus {
 	public static void main(String[] args) {
 		System.out.println("Hello Dionychus");
-		System.out.println("Dédicace de ChienDesBois");
+		System.out.println("Bonjour tout le monde");
 		System.out.println("MERDE");
 	}
 }
