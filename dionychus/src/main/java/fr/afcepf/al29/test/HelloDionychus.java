f4f9f811fc96e621f82c92b895926dea8a73d8a2
chienDesbois <vincentzanol@gmail.com>
2016-12-07 13:08:47 +0100

    VZ 13.08

--- dionychus/src/main/java/fr/afcepf/al29/test/HelloDionychus.java
+++ dionychus/src/main/java/fr/afcepf/al29/test/HelloDionychus.java
@@ -3,21 +3,11 @@ package fr.afcepf.al29.test;
 public class HelloDionychus {
 	public static void main(String[] args) {
 		System.out.println("Hello Dionychus");
-<<<<<<< HEAD
-<<<<<<< HEAD
 		System.out.println("GIT ne fonctionne pas ce matin !!");
-=======
-		System.out.println("D�dicace de ChienDesBois");
-=======
->>>>>>> 66cc3703090d5903110470173e36ce2aee80727c
 		System.out.println("Change ton putain d'encodage sale chienDesBois");
 		System.out.println("MERDE");
 		System.out.println("Bonjour tout le monde");
 		System.out.println("MERDE à la crème");
-<<<<<<< HEAD
->>>>>>> 5daec91f8371bafc1e6804c2b5565ca6fb20f3f9
-=======
 		System.out.println("test conflit");
->>>>>>> 66cc3703090d5903110470173e36ce2aee80727c
 	}
 }
