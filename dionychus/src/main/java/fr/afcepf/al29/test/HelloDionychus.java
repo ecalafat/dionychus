67fffb4239eca2e35357401014b505ceb092c2fd
AntoineHZ <antoine.huet@gmx.com>
2016-12-07 12:06:36 +0100

    commit AHZ

--- dionychus/src/main/java/fr/afcepf/al29/test/HelloDionychus.java
+++ dionychus/src/main/java/fr/afcepf/al29/test/HelloDionychus.java
@@ -3,5 +3,6 @@ package fr.afcepf.al29.test;
 public class HelloDionychus {
 	public static void main(String[] args) {
 		System.out.println("Hello Dionychus");
+		System.out.println("GIT ne fonctionne pas ce matin !!");
 	}
 }
