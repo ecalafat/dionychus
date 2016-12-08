55fecf1ebedef2bf53073b848d1cb10c3d4c5957
ecalafat <e.calafat@gmail.com>
2016-12-08 16:38:00 +0100

    test ecraser helloDionychus

--- dionychus/src/main/java/fr/afcepf/al29/test/HelloDionychus.java
+++ dionychus/src/main/java/fr/afcepf/al29/test/HelloDionychus.java
@@ -3,15 +3,5 @@ package fr.afcepf.al29.test;
 public class HelloDionychus {
 	public static void main(String[] args) {
 		System.out.println("Hello Dionychus");
-<<<<<<< HEAD
-=======
-		System.out.println("GIT ne fonctionne pas ce matin !!");
-		System.out.println("Change ton putain d'encodage sale chienDesBois");
-		System.out.println("MERDE");
-		System.out.println("Bonjour tout le monde");
-		System.out.println("MERDE à la crème");
-		System.out.println("test conflit");
-		System.out.println("Test AHZ 13.05");
->>>>>>> 0d6931934704a1e1e45b19a2f20d84970e0fc5dd
 	}
 }
